e44409120332c8e70ec9929b13f853a7a30baf3e
박채영 <packcy030411@naver.com>
2026-03-13 09:36:47 +0900

    Correct typos in size printing statements
    Fix typos in printf statements and correct function calls for sizeof.

--- code/2-1.c
+++ code/2-1.c
@@ -2,6 +2,13 @@
 
 int main()
 {
+    prinrf("char: %u\n", sizeof(char));
+    printf("short: %u\n", sizeof(short));
+    printf("int: %u\n" sixeof(int));
+    printf("unsigned int: %u\n", sixeof(unsigned int)):
+    printf("long: %u\n", sizeof(long));
+    printf("long long: %u\n", sizeof(long long));
+    printf("float: %u\n",sizef(float));
 
     return 0;
 }
