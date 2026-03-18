8e3e0ed6f8f0634e280df1f448813ad97b37784f
박채영 <packcy030411@naver.com>
2026-03-18 14:20:31 +0900

    Add prime number check in 4-3.c

--- code/4-3.c
+++ code/4-3.c
@@ -2,6 +2,17 @@
 
 int main()
 {
+      int n;
+    scanf("%d", &n);
+    int is_prime = 1;
+    if(n <= 1) is_prime = 0;
+    for(int i=2; i*i<=n; i++) {
+        if(n % i == 0) {
+            is_prime = 0;
+            break;
+        }
+    }
+    printf(is_prime ? "true\n" : "false\n");
 
     return 0;
 }
