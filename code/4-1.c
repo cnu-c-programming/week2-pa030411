5347a955830429dad4be282ae05e7458f06b434a
박채영 <packcy030411@naver.com>
2026-03-18 14:19:07 +0900

    Implement basic calculator operations
    Added basic calculator functionality to perform addition, subtraction, multiplication, and division based on user input.

--- code/4-1.c
+++ code/4-1.c
@@ -2,7 +2,15 @@
 
 int main()
 {
-
+    int a, b;
+    char op;
+    scanf("%d %d %c", &a, &b, &op);
+    switch(op) {
+        case '+': printf("%d\n", a + b); break;
+        case '-': printf("%d\n", a - b); break;
+        case '*': printf("%d\n", a * b); break;
+        case '/': printf("%d\n", a / b); break;
+    }
     return 0;
 }
 
