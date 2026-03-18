090b3c50aa3a46340f41e6f1f7a8d9076a7faff0
박채영 <packcy030411@naver.com>
2026-03-18 14:14:31 +0900

    Update 2-1.c

--- code/2-1.c
+++ code/2-1.c
@@ -2,13 +2,14 @@
 
 int main()
 {
-    prinrf("char: %u\n", sizeof(char));
-    printf("short: %u\n", sizeof(short));
-    printf("int: %u\n" sixeof(int));
-    printf("unsigned int: %u\n", sixeof(unsigned int)):
-    printf("long: %u\n", sizeof(long));
-    printf("long long: %u\n", sizeof(long long));
-    printf("float: %u\n",sizef(float));
+    printf("char: %zu\n", sizeof(char));
+    printf("short: %zu\n", sizeof(short));
+    printf("int: %zu\n", sizeof(int));
+    printf("unsigned int: %zu\n", sizeof(unsigned int));
+    printf("long: %zu\n", sizeof(long));
+    printf("long long: %zu\n", sizeof(long long));
+    printf("float: %zu\n", sizeof(float));
+
 
     return 0;
 }
